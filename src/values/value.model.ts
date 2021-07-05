afbcfc141eeb2bb855c3bdabae05cb13a001adb7
David Crandall <davecrands@gmail.com>
2021-07-05 13:04:45 -0500

    Add activities ref to value schema

--- src/values/value.model.ts
+++ src/values/value.model.ts
@@ -4,12 +4,14 @@ export interface IValue {
     name: string;
     description: string;
     importance: number;
+    activities: string[];
 }
 
 const schema = new Schema<IValue>({
     name: { type: String, required: true },
     description: { type: String, required: true },
     importance: { type: Number, required: true },
+    activities: [{ type: Schema.Types.ObjectId, ref: 'Activity' }]
 });
 
 export const Value = model<IValue>('Value', schema);
