f3505619e0a8c48c7bd53b9e16982df88f62e649
David Crandall <davecrands@gmail.com>
2021-07-04 21:13:01 -0500

    Use mongoose in value interface

--- src/values/value.interface.ts
+++ src/values/value.interface.ts
@@ -1,9 +1,15 @@
-export interface BaseValue {
+import { Schema, model } from 'mongoose';
+
+export interface IValue {
     name: string;
     description: string;
     importance: number;
 }
 
-export interface Value extends BaseValue {
-    id: number;
-}
+const schema = new Schema<IValue>({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    importance: { type: Number, required: true },
+});
+
+export const Value = model<IValue>('Value', schema);
